3cefa80d76dd213a776294b76f8a106557286483
egittoarabia5-a11y <egittoarabia5@gmail.com>
2025-09-27 11:12:41 +0200

    update1.4

--- server.js
+++ server.js
@@ -194,8 +194,8 @@ app.get('/torinometeo.json', async (req, res) => {
 // --- Endpoint Meteo3R ---
 app.get('/meteo3r.json', async (req, res) => {
   try {
-    const url = "https://api.allorigins.win/raw?url=" + 
-    encodeURIComponent("https://www.meteo3r.it/dati/mappe/misure.geojson");
+    const url = "https://thingproxy.freeboard.io/fetch/" + 
+    "https://www.meteo3r.it/dati/mappe/misure.geojson";
   
 
     const response = await fetch(url);
