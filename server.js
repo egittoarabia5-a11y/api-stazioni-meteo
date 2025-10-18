b22867ec2c5abaa5277e7ab12055a96d7aecdaba
egittoarabia5-a11y <egittoarabia5@gmail.com>
2025-10-19 00:24:29 +0200

    update13.11

--- server.js
+++ server.js
@@ -359,7 +359,7 @@ const stationsNetAtmo = [ // Zona Genova Ovest / Centro
       "via federico avio": 0.5, "via antonio caveri": 2.0, "via dante gaetano storace": 3.0, "via dei landi": 2.0,
       "via gian battista monti": 1.5, "via robert baden powell": 2.0, "salita al forte della crocetta": 3.0,
       "via guido agosti": 2.0, "via dei sessanta": 2.0, "via ludovico calda": 1.0, "genoa": 2.0,
-      "via san pantaleo": 4.0, "via apparizione": 0.5, "via borgoratti": 1.5,
+      "via san pantaleo": 4.0, "via apparizione": 0.5, "via borgoratti": 1.5, "A12": 3.5,
       "via fratelli canale": 0.5, "via eleonora duse": 1.5, "viale gambaro": 1.0, "via puggia": 1.0, "via bartolomeo bianco": 2.0,
     };
 
